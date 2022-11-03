13d8f02c9aecb8673990d02722a6405b5582e5db
Chris <ghost@drednaut.net>
2022-11-03 11:58:35 -0700

    init
    Signed-off-by: Chris <ghost@drednaut.net>

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+	return (
+		<div>
+			<h1>Admin</h1>
+		</div>
+	);
+}
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import './globals.css';
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html>
+			<head></head>
+			<body>
+				<main>
+					<nav>
+						<Link href="/">Home</Link>
+						<Link href="/admin">Admin</Link>
+					</nav>
+					{children}
+				</main>
+			</body>
+		</html>
+	);
+}
